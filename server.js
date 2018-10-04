0afe502e72d9ffef9412309173987ca994063d98
nickfp1985 <nfpeddie@gmail.com>
2018-10-03 17:24:28 -0700

    decided to start over from scratch, replaced all files with react/express/node boilerplate from class repo

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const routes = require("./routes");
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,13 +11,15 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// Add routes, both API and view
-app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nytreact");
+// Define API routes here
 
-// Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
